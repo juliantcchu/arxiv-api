53316173cd94bc732ff1335533bb0822a00cd630
Julian Chu <juliantcchu@gmail.com>
2023-01-26 16:25:24 -0800

    Update arxiv_api.py

--- arxiv_api.py
+++ arxiv_api.py
@@ -1,14 +1,23 @@
-import urllib, urllib.request
-import xml.etree.ElementTree as Xet
-from xml.etree.ElementTree import fromstring, ElementTree
 import requests as r
 import pandas as pd
-
+import json
+import xmltodict
 
 def print_xml_tree(root, level=0):
     for child in root:
         print(''.join(['--' for i in range(level)]), child.tag, '||', child.text)
         print_xml_tree(child, level=level+1)
+
+
+
+
+def parse_dict_or_list(obj, key):
+    string = ''
+    if type(obj) == type([]):
+        string = ', '.join([a.get(key, '') for a in obj])
+    else:
+        string = obj.get(key, '')
+    return string
 
 
 def get_arxiv_data(q):
@@ -21,37 +30,30 @@ def get_arxiv_data(q):
     })
     print('parsing data...')
     data = data.content.decode('utf-8')
-    cols = ["title", "published", 'author', "summary",]
+    data = xmltodict.parse(data)
+    
+    print(json.dumps(data, indent=4))
+
     rows = []
-    # print(data)
+
+    entries = data['feed']['entry']
+    for entry in entries:
+
+        rows.append({
+            "id": entry["id"],
+            "title": entry["title"], 
+            "published": entry["published"],
+            "author": parse_dict_or_list(entry['author'], 'name'),#author,
+            "summary": entry['summary'],
+            "link": entry["id"],
+            "category": parse_dict_or_list(entry['category'], '@term'),
+        })
+    
+    print(rows)
     
 
-    # Parsing the XML file
-    root = ElementTree(fromstring(data)).getroot()
-    print('meta data: ')
-    for row in root[:7]:
-        start_ind = row.tag.find('}')
-        print('--', row.tag[start_ind+1 if start_ind != -1 else 0 :], ': ', row.text)
-
-    # print_xml_tree(root)
-
-    for child in root:
-        if 'entry' in child.tag:
-            row = {}
-            names = []
-            for elem in child:
-                for c in cols:
-                    if 'author' in elem.tag:
-                        for name in elem:
-                            names.append(name.text)
-                        break
-                    elif c in elem.tag:
-                        row[c] = elem.text
-                        break
-            row['author'] = ','.join(names)
-            rows.append(row)
  
-    df = pd.DataFrame(rows, columns=cols)
+    df = pd.DataFrame(rows)
     return df
 
 
